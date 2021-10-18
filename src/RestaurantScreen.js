a5096d14b60343af0ffc4df55a0a947488c98f69
sartain <alex.sartain@zuhlke.com>
2021-10-18 10:06:06 +0100

    fix

--- src/RestaurantScreen.js
+++ src/RestaurantScreen.js
@@ -1,9 +1,13 @@
 import React from "react";
 import RestaurantList from "./RestaurantList.js";
+const restaurants = [
+  { id: 1, name: "Sushi Place" },
+  { id: 2, name: "Pizza Place" },
+];
 const RestaurantScreen = () => (
   <div>
     <h1>Restaurants</h1>
-    <RestaurantList />
+    <RestaurantList loadRestaurants={() => {}} restaurants={restaurants} />
   </div>
 );
 export default RestaurantScreen;
